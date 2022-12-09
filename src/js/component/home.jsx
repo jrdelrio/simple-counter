b31ef29375d4d4e678a63024c4deadc2e0e81f87
Jose Raimundo Del Rio <jrdelriodom@gmail.com>
2022-12-09 23:37:32 +0000

    falta que no colapse el contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,82 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState }  from "react";
 
 //create your first component
 const Home = () => {
+
+	let [secondsUni, setSecondsUni] = useState(0);
+	let [secondsDec, setSecondsDec] = useState(0);
+	let [minutesUni, setMinutesUni] = useState(0);
+	let [minutesDec, setMinutesDec] = useState(0);
+	let [hoursUni, setHoursUni] = useState(0);
+	let [hoursDec, setHoursDec] = useState(0);
+
+	const change = () => {
+		setSecondsUni(secondsUni += 1);
+
+		if(secondsUni === 10){
+			setSecondsUni(secondsUni = 0);
+			setSecondsDec(secondsDec += 1);
+		};
+	
+		if(secondsDec === 6){
+			setSecondsDec(secondsDec = 0);
+			setSecondsUni(secondsUni = 0);
+			setMinutesUni(minutesUni += 1);
+		};
+	
+		if(minutesUni === 10){
+			setMinutesUni(minutesUni = 0);
+			setSecondsDec(secondsDec = 0);
+			setSecondsUni(secondsUni = 0);
+			setMinutesDec(minutesDec += 1);
+		};
+	
+		if(minutesDec === 6){
+			setMinutesDec(minutesDec = 0);
+			setMinutesUni(minutesUni = 0);
+			setSecondsDec(secondsDec = 0);
+			setSecondsUni(secondsUni = 0);
+			setHoursUni(hoursUni += 1);
+		};
+	
+		if(hoursUni === 10){
+			setHoursUni(hoursUni = 0);
+			setMinutesDec(minutesDec = 0);
+			setMinutesUni(minutesUni = 0);
+			setSecondsDec(secondsDec = 0);
+			setSecondsUni(secondsUni = 0);
+			setHoursDec(hoursDec += 1);
+		};
+	
+		if(hoursDec == 6){
+			setHoursDec(hoursDec = 0);
+			setHoursUni(hoursUni = 0);
+			setMinutesDec(minutesDec = 0);
+			setMinutesUni(minutesUni = 0);
+			setSecondsDec(secondsDec = 0);
+			setSecondsUni(secondsUni = 0);
+		};
+
+
+	}
+
+	setInterval(change, 3000);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="wrapper">
+			<div className="full-clock">
+				<p className="digit"><span class="material-symbols-outlined">schedule</span></p>
+				<p className="digit">{hoursDec}</p>
+				<p className="digit">{hoursUni}</p>
+				<p className="digit">{minutesDec}</p>
+				<p className="digit">{minutesUni}</p>
+				<p className="digit">{secondsDec}</p>
+				<p className="digit">{secondsUni}</p>
+					
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
